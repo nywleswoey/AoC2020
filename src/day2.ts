98ff5f58b98e5bec2368d6d20a10ae22e4797fb7
Selwyn Yeow <sgfjords@gmail.com>
2020-12-02 14:11:51 +0800

    Implement getNumOfValidPasswords

--- src/day2.ts
+++ src/day2.ts
@@ -2,4 +2,23 @@ const parseLine = (s: string) => {
   return s.split(/(?:-|\s|:\s)/);
 };
 
-export { parseLine };
+const parseInput = (input: string) => {
+  const lines = input.split(/\n/);
+
+  const parsedLines = lines.map(parseLine);
+  return parsedLines;
+};
+
+const isPasswordValid = (parsedLine: string[]) => {
+  const min = Number(parsedLine[0]);
+  const max = Number(parsedLine[1]);
+  const numOccurences = parsedLine[3].split(parsedLine[2]).length - 1;
+  return numOccurences >= min && numOccurences <= max;
+};
+
+const getNumOfValidPasswords = (input: string) => {
+  const passwordCheckStatuses = parseInput(input).map(isPasswordValid);
+  return passwordCheckStatuses.filter((x) => x).length;
+};
+
+export { parseLine, getNumOfValidPasswords };
