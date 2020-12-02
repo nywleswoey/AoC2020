c6164053fa04d9dae45ddd50b655c90c060a317f
Selwyn Yeow <sgfjords@gmail.com>
2020-12-02 12:12:51 +0800

    Solve day 1 puzzle 1

--- src/day1.ts
+++ src/day1.ts
@@ -1,5 +1,13 @@
 const findProduct = (inputs: number[]) => {
-  return 514579;
+  const seenNumbers = new Map<number, boolean>();
+
+  for (const input of inputs) {
+    const complementNumber = 2020 - input;
+    if (seenNumbers.get(complementNumber)) {
+      return input * complementNumber;
+    }
+    seenNumbers.set(input, true);
+  }
 };
 
 export { findProduct };
