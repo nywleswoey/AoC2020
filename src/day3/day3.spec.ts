3d0f75442c0c4ca9b43d1bdb7847290414e7e258
Selwyn Yeow <sgfjords@gmail.com>
2020-12-03 14:13:52 +0800

    Add test for day 3 puzzle 2

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -3,11 +3,31 @@ import { getNumTreesEncountered } from "./day3";
 
 const testInput = readFile("inputs/day3/testInput.txt");
 
-describe("day 3 puzzle 1", () => {
+describe("day 3", () => {
   describe("getNumTreesEncountered", () => {
-    it("should return 7 when given the test input", () => {
-      const result = getNumTreesEncountered(testInput, 3);
+    it("should return 7 when given the test input with slope 3,1", () => {
+      const result = getNumTreesEncountered(testInput, 3, 1);
       expect(result).toBe(7);
+    });
+
+    it("should return 2 when given the test input with slope 1,1", () => {
+      const result = getNumTreesEncountered(testInput, 1, 1);
+      expect(result).toBe(2);
+    });
+
+    it("should return 3 when given the test input with slope 5,1", () => {
+      const result = getNumTreesEncountered(testInput, 5, 1);
+      expect(result).toBe(3);
+    });
+
+    it("should return 4 when given the test input with slope 7,1", () => {
+      const result = getNumTreesEncountered(testInput, 7, 1);
+      expect(result).toBe(4);
+    });
+
+    it("should return 2 when given the test input with slope 1,2", () => {
+      const result = getNumTreesEncountered(testInput, 1, 2);
+      expect(result).toBe(2);
     });
   });
 });
