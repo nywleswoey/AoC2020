b1cfd6ab8a269fd361c20cee8101719b945c3608
Selwyn Yeow <sgfjords@gmail.com>
2020-12-03 13:47:05 +0800

    Add test for day 3 puzzle 1

---
+++ src/day3/day3.spec.ts
@@ -0,0 +1,13 @@
+import { readFile } from "../helper";
+import { getNumTreesEncountered } from "./day3";
+
+const testInput = readFile("inputs/day3/testInput.txt");
+
+describe("day 2 puzzle 1", () => {
+  describe("getNumTreesEncountered", () => {
+    it("should return 7 when given the test input", () => {
+      const result = getNumTreesEncountered(testInput, 3);
+      expect(result).toBe(7);
+    });
+  });
+});
