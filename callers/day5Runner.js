d22b3a013c79434113618fd1ef14284928ff8205
Selwyn Yeow <sgfjords@gmail.com>
2020-12-05 15:48:45 +0800

    Solve day 5 puzzle 2

--- callers/day5Runner.js
+++ callers/day5Runner.js
@@ -7,3 +7,13 @@ const maxSeatId = boardingPassDetails.reduce((p, c) => {
   return Math.max(p, c[2]);
 }, 0);
 console.log(maxSeatId);
+
+const seatIds = boardingPassDetails.map((x) => x[2]);
+const sortedSeatIds = seatIds.sort((a, b) => a - b);
+const offset = sortedSeatIds[0];
+for (let x = 0; x < sortedSeatIds.length; x++) {
+  if (sortedSeatIds[x] !== offset + x) {
+    console.log(offset + x);
+    break;
+  }
+}
