a3f05cf11674fd457409d7c4723bbb571da97c52
Selwyn Yeow <sgfjords@gmail.com>
2020-12-06 13:23:26 +0800

    Add test for day 6 puzzle 2

--- src/day6/day6.spec.ts
+++ src/day6/day6.spec.ts
@@ -38,3 +38,42 @@ describe("day 6 puzzle 1", () => {
     });
   });
 });
+
+describe("day 6 puzzle 2", () => {
+  describe("countNumAllYesPerGrp", () => {
+    it("test 1", () => {
+      const input = ['abc'];
+
+      const result = countNumAllYesPerGrp(input);
+      expect(result).toBe(3);
+    });
+
+    it("test 2", () => {
+      const input = ['a', 'b', 'c'];
+
+      const result = countNumAllYesPerGrp(input);
+      expect(result).toBe(0);
+    });
+
+    it("test 3", () => {
+      const input = ['ab', 'ac'];
+
+      const result = countNumAllYesPerGrp(input);
+      expect(result).toBe(1);
+    });
+
+    it("test 4", () => {
+      const input = ['a', 'a', 'a', 'a'];
+
+      const result = countNumAllYesPerGrp(input);
+      expect(result).toBe(1);
+    });
+
+    it("test 5", () => {
+      const input = ['b'];
+
+      const result = countNumAllYesPerGrp(input);
+      expect(result).toBe(1);
+    });
+  });
+});
