cc716a97d799c0e2a477216dabf79977af53c584
Selwyn Yeow <sgfjords@gmail.com>
2020-12-08 14:08:54 +0800

    Add test for day 8 puzzle 2

--- src/day8/day8.spec.ts
+++ src/day8/day8.spec.ts
@@ -1,12 +1,22 @@
 import { readFile } from "../helper";
-import { findAccBeforeLoop } from './day8';
+import { findAccBeforeLoop, findAccAfterProgEnd } from './day8';
 
 describe("day 8 puzzle 1", () => {
   describe("findAccBeforeLoop", () => {
     it("test 1", () => {
       const instructions = readFile("inputs/day8/testInput.txt");
       const result = findAccBeforeLoop(instructions);
-      expect(result).toBe(5);
+      expect(result[0]).toBe(5);
+    });
+  });
+});
+
+describe("day 8 puzzle 2", () => {
+  describe("findAccAfterProgEnd", () => {
+    it("test 1", () => {
+      const instructions = readFile("inputs/day8/testInput.txt");
+      const result = findAccAfterProgEnd(instructions);
+      expect(result).toBe(8);
     });
   });
 });
