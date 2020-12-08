6d2a35a89ef3a3bfe6533fb8e9ff60531d1c83c2
Selwyn Yeow <sgfjords@gmail.com>
2020-12-08 08:42:46 +0800

    Add test for day 7 puzzle 2

--- src/day7/day7.spec.ts
+++ src/day7/day7.spec.ts
@@ -1,5 +1,5 @@
 import { readFile } from "../helper";
-import { getBagCountContainingWantedBag } from './day7';
+import { getBagCountContainingWantedBag, computeNumBagsInBag } from './day7';
 
 describe("day 7 puzzle 1", () => {
   describe("getBagCountContainingWantedBag", () => {
@@ -7,6 +7,22 @@ describe("day 7 puzzle 1", () => {
       const bagDetails = readFile("inputs/day7/testInput.txt");
       const result = getBagCountContainingWantedBag(bagDetails, 'shiny gold');
       expect(result).toBe(4);
+    });
+  });
+});
+
+describe("day 7 puzzle 2", () => {
+  describe("computeNumBagsInBag", () => {
+    it("computeNumBagsInBag test 1", () => {
+      const bagDetails = readFile("inputs/day7/testInput.txt");
+      const result = computeNumBagsInBag(bagDetails, 'shiny gold');
+      expect(result).toBe(32);
+    });
+
+    it("computeNumBagsInBag test 2", () => {
+      const bagDetails = readFile("inputs/day7/testInput2.txt");
+      const result = computeNumBagsInBag(bagDetails, 'shiny gold');
+      expect(result).toBe(126);
     });
   });
 });
