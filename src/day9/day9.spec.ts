62b8eaf78b21302fd183dde5f54b3a52aedf18ab
Selwyn Yeow <sgfjords@gmail.com>
2020-12-09 13:51:58 +0800

    Add test for day 9 puzzle 2

--- src/day9/day9.spec.ts
+++ src/day9/day9.spec.ts
@@ -1,12 +1,22 @@
 import { readFile } from "../helper";
-import { findInvalidNumber } from './day9';
+import { findInvalidNumber, findSumOfMinMaxInContiguousBlock } from './day9';
+
+const input = readFile("inputs/day9/testInput.txt").map(Number);
 
 describe("day 9 puzzle 1", () => {
   describe("findInvalidNumber", () => {
     it("test 1", () => {
-      const input = readFile("inputs/day9/testInput.txt").map(Number);
       const result = findInvalidNumber(input, 5);
       expect(result).toBe(127);
+    });
+  });
+});
+
+describe("day 9 puzzle 2", () => {
+  describe("findSumOfMinMaxInContiguousBlock", () => {
+    it("test 1", () => {
+      const result = findSumOfMinMaxInContiguousBlock(input, 127);
+      expect(result).toBe(62);
     });
   });
 });
