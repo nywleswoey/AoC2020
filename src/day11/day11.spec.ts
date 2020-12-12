33dcdc978e21937e20f1f331a6dca69f5fb31327
Selwyn Yeow <sgfjords@gmail.com>
2020-12-12 09:33:02 +0800

    Add test for day 11 puzzle 2

--- src/day11/day11.spec.ts
+++ src/day11/day11.spec.ts
@@ -1,5 +1,5 @@
 import { readFileInto2DCharArray } from "../helper";
-import { findNumOccupiedSeatsInEquilibrium } from './day11';
+import { findNumOccupiedSeatsInEquilibrium, findNumOccupiedSeatsInEquilibrium2 } from './day11';
 
 
 describe("day 11 puzzle 1", () => {
@@ -9,6 +9,17 @@ describe("day 11 puzzle 1", () => {
 
       const result = findNumOccupiedSeatsInEquilibrium(input);
       expect(result).toBe(37);
+    });
+  });
+});
+
+describe("day 11 puzzle 2", () => {
+  describe("findNumOccupiedSeatsInEquilibrium2", () => {
+    it("test 1", () => {
+      const input = readFileInto2DCharArray("inputs/day11/testInput.txt");
+
+      const result = findNumOccupiedSeatsInEquilibrium2(input);
+      expect(result).toBe(26);
     });
   });
 });
