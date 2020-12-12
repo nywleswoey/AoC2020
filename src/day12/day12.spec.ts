45909aad10e4f9ab752e1aed31f19b156d0ddf8b
Selwyn Yeow <sgfjords@gmail.com>
2020-12-12 13:38:06 +0800

    Add test for day 12 puzzle 2

--- src/day12/day12.spec.ts
+++ src/day12/day12.spec.ts
@@ -1,5 +1,5 @@
 import { readFile } from "../helper";
-import { findManhattanDistance } from './day12';
+import { findManhattanDistance, findManhattanDistance2 } from './day12';
 
 
 describe("day 12 puzzle 1", () => {
@@ -9,6 +9,17 @@ describe("day 12 puzzle 1", () => {
 
       const result = findManhattanDistance(input);
       expect(result).toBe(25);
+    });
+  });
+});
+
+describe("day 12 puzzle 2", () => {
+  describe("findManhattanDistance", () => {
+    it("test 1", () => {
+      const input = readFile("inputs/day12/testInput.txt");
+
+      const result = findManhattanDistance2(input);
+      expect(result).toBe(286);
     });
   });
 });
