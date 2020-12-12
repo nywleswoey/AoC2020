f5ab61d50fac8dfa8f45a428caca568d2738b494
Selwyn Yeow <sgfjords@gmail.com>
2020-12-12 11:18:20 +0800

    Solve day 11 puzzle 2

--- callers/day11Runner.js
+++ callers/day11Runner.js
@@ -4,3 +4,5 @@ const testInput = helpers.readFileInto2DCharArray("inputs/day11/puzzleInput.txt"
 
 let input = testInput;
 console.log(day11.findNumOccupiedSeatsInEquilibrium(input));
+
+console.log(day11.findNumOccupiedSeatsInEquilibrium2(input));
--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -94,4 +94,202 @@ const findNumOccupiedSeatsInEquilibrium = (input: string[][]) => {
   }
 }
 
-export { findNumOccupiedSeatsInEquilibrium };
+const getAffectingSeatKeys2 = (input: any[][], width: number, height: number, x: number, y: number) => {
+  const result = new Array<string>();
+  let coordX = x, coordY = y, i = 0;
+  let isSeatFound = false;
+
+  while (!isSeatFound) {
+    i++
+    // diagonal upper left direction
+    coordX = x - i;
+    coordY = y - i;
+    if (coordY < 0 || coordY < 0) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // up direction
+    coordX = x;
+    coordY = y - i;
+    if (coordY < 0) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // diagonal upper right direction
+    coordX = x + i;
+    coordY = y - i;
+    if (coordX >= width || coordY < 0) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // left direction
+    coordX = x - i;
+    coordY = y;
+    if (coordX < 0) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // right direction
+    coordX = x + i;
+    coordY = y;
+    if (coordX >= width) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // diagonal lower left direction
+    coordX = x - i;
+    coordY = y + i;
+    if (coordY < 0 || coordY >= height) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // down direction
+    coordX = x;
+    coordY = y + i;
+    if (coordY >= height) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  isSeatFound = false;
+  i = 0;
+  while (!isSeatFound) {
+    i++
+    // diagonal lower right direction
+    coordX = x + i;
+    coordY = y + i;
+    if (coordX >= width || coordY >= height) {
+      break;
+    }
+
+    if (input[coordY][coordX] === 'L') {
+      result.push(`${coordX},${coordY}`);
+      isSeatFound = true;
+    }
+  }
+
+  return result;
+}
+
+const getSeatMap2 = (input: string[][]) => {
+  const seatMap = new Map<string, ISeat>();
+  const height = input.length;
+  const width = input[0].length;
+
+  for (let rowIndex = 0; rowIndex < height; rowIndex++) {
+    for (let colIndex = 0; colIndex < width; colIndex++) {
+      if (input[rowIndex][colIndex] === 'L') {
+        seatMap.set(`${colIndex},${rowIndex}`, { isOccupied: false, hasChanged: false, affectingSeatKeys: [] });
+      }
+    }
+  }
+
+  for (const [key, seat] of seatMap) {
+    const coords = key.split(',').map(Number);
+    seat.affectingSeatKeys = getAffectingSeatKeys2(input, width, height, coords[0], coords[1]);
+    seatMap.set(key, seat);
+  }
+
+  return seatMap;
+}
+
+const applyRules2 = (seatMap: Map<string, ISeat>) => {
+  const newSeatMap = new Map<string, ISeat>();
+  let willChange = false;
+  for (const [key, seat] of seatMap) {
+    const { isOccupied, affectingSeatKeys } = seat;
+    const affectingSeats = affectingSeatKeys.map(k => seatMap.get(k)!);
+    let newSeat = { ...seat };
+    if (isOccupied) {
+      // will change if 5 or more affecting seats are occupied
+      willChange = affectingSeats.filter(s => s.isOccupied).length > 4;
+      newSeat = { ...newSeat, isOccupied: !willChange, hasChanged: willChange };
+    } else {
+      // will change if there are no affecting seats that are occupied
+      willChange = affectingSeats.filter(s => s.isOccupied).length === 0;
+      newSeat = { ...newSeat, isOccupied: willChange, hasChanged: willChange };
+    }
+    newSeatMap.set(key, newSeat);
+  }
+
+  return newSeatMap;
+}
+
+const findNumOccupiedSeatsInEquilibrium2 = (input: string[][]) => {
+  let seatMap = getSeatMap2(input);
+
+  while (true) {
+    seatMap = applyRules2(seatMap);
+    const [occupiedCount, hasChanged] = checkSeatMap(seatMap);
+    if (!hasChanged) {
+      return occupiedCount;
+    }
+  }
+}
+
+export { findNumOccupiedSeatsInEquilibrium, findNumOccupiedSeatsInEquilibrium2 };
