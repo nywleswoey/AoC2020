8a8afc51360a9d43d0a5ae94bcec0dde849d9e06
Selwyn Yeow <sgfjords@gmail.com>
2020-12-14 21:45:32 +0800

    Add test for day 13 puzzle 2

--- src/day13/day13.spec.ts
+++ src/day13/day13.spec.ts
@@ -1,5 +1,5 @@
 import { readFile } from "../helper";
-import { productBusIDAndWaitTime } from './day13';
+import { productBusIDAndWaitTime, findTimeWhereBusArriveInSeq } from './day13';
 
 
 describe("day 13 puzzle 1", () => {
@@ -11,6 +11,26 @@ describe("day 13 puzzle 1", () => {
 
       const result = productBusIDAndWaitTime(time, buses);
       expect(result).toBe(295);
+    });
+  });
+});
+
+describe("day 13 puzzle 2", () => {
+  describe("findTimeWhereBusArriveInSeq", () => {
+    it("test 1", () => {
+      const input = readFile("inputs/day13/testInput.txt");
+      const buses = input[1].split(',');
+
+      const result = findTimeWhereBusArriveInSeq(buses);
+      expect(result).toBe(1068781);
+    });
+
+    it("test 2", () => {
+      const input = readFile("inputs/day13/testInput2.txt");
+      const buses = input[0].split(',');
+
+      const result = findTimeWhereBusArriveInSeq(buses);
+      expect(result).toBe(3417);
     });
   });
 });
