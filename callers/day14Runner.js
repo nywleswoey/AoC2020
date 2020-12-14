380ea5295fcc6c3c3572e4f5f60cbbf1c4e9bdac
Selwyn Yeow <sgfjords@gmail.com>
2020-12-14 14:39:13 +0800

    Solve day 14 puzzle 2

--- callers/day14Runner.js
+++ callers/day14Runner.js
@@ -2,3 +2,5 @@ const { helpers, day14 } = require("../dist/index");
 
 const testInput = helpers.readFile("inputs/day14/puzzleInput.txt");
 console.log(day14.findSumOfAddresses(testInput));
+
+console.log(day14.findSumOfAddresses2(testInput));
--- src/day14/day14.ts
+++ src/day14/day14.ts
@@ -41,4 +41,65 @@ const findSumOfAddresses = (input: string[]) => {
   return sum;
 }
 
-export { findSumOfAddresses };
+const convertMaskToMap2 = (mask: string) => {
+  const replacementMap = new Map<number, string>();
+  for (let i = 0; i < mask.length; i++) {
+    replacementMap.set(i, mask[i])
+  }
+
+  return replacementMap;
+}
+
+const applyMask2 = (replacementMap: Map<number, string>, bitValue: string) => {
+  let bits = bitValue.split('');
+  for (const [k, v] of replacementMap) {
+    if (v !== '0') {
+      bits[k] = v;
+    }
+  }
+  return bits.join('');
+}
+
+const findAddresses = (bitArr: string[]): string[] => {
+  const index = bitArr.indexOf('X');
+  if (index === -1) {
+    return [bitArr.join('')];
+  }
+
+  const bitArr1 = [...bitArr], bitArr2 = [...bitArr];
+  bitArr1[index] = '0';
+  bitArr2[index] = '1'
+
+  return [...findAddresses(bitArr1), ...findAddresses(bitArr2)];
+}
+
+const findSumOfAddresses2 = (input: string[]) => {
+  const addressMap = new Map<number, number>();
+  let replacementMap = new Map<number, string>();
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].startsWith('mask')) {
+      // udpate mask
+      replacementMap = convertMaskToMap2(input[i].replace('mask = ', ''));
+    } else {
+      const tokens = input[i].split('] = ');
+      const memAdd = Number(tokens[0].replace('mem[', '')).toString(2).padStart(36, '0');
+      const value = Number(tokens[1]);
+
+      const memBitArrAfterApplyingMask = applyMask2(replacementMap, memAdd).split('');
+      const bitAddresses = findAddresses(memBitArrAfterApplyingMask);
+
+      for (const bitAddress of bitAddresses) {
+        addressMap.set(parseInt(bitAddress, 2), value);
+      }
+    }
+  }
+
+  let sum = 0;
+  for (const [k, v] of addressMap) {
+    sum += v;
+  }
+  return sum;
+}
+
+export { findSumOfAddresses, findSumOfAddresses2 };
