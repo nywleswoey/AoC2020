5b09bb3669caa71b8ea51dc52e127075eb6a0de7
Selwyn Yeow <sgfjords@gmail.com>
2020-12-15 13:35:57 +0800

    Add test for day 15 puzzle 2

--- src/day15/day15.spec.ts
+++ src/day15/day15.spec.ts
@@ -1,19 +1,34 @@
 import { readFile } from "../helper";
-import { get2020thNumber } from './day15';
+import { getnthNumber } from './day15';
 
-
-describe("day 14 puzzle 1", () => {
+describe("day 15 puzzle 1", () => {
   describe("get2020thNumber", () => {
     it("test 1", () => {
       const input = readFile("inputs/day15/testInput.txt")[0].split(',').map(Number);
-      const result = get2020thNumber(input);
+      const result = getnthNumber(input, 2020);
       expect(result).toBe(436);
     });
 
     it("test 2", () => {
       const input = readFile("inputs/day15/testInput2.txt")[0].split(',').map(Number);
-      const result = get2020thNumber(input);
+      const result = getnthNumber(input, 2020);
       expect(result).toBe(1);
+    });
+  });
+});
+
+describe("day 15 puzzle 2", () => {
+  describe("get3000000thNumber", () => {
+    it("test 1", () => {
+      const input = readFile("inputs/day15/testInput.txt")[0].split(',').map(Number);
+      const result = getnthNumber(input, 30000000);
+      expect(result).toBe(175594);
+    });
+
+    it("test 2", () => {
+      const input = readFile("inputs/day15/testInput2.txt")[0].split(',').map(Number);
+      const result = getnthNumber(input, 30000000);
+      expect(result).toBe(2578);
     });
   });
 });
