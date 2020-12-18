36cb10ed3fb5d2e10597472cae9bb4ab8e74391f
Selwyn Yeow <sgfjords@gmail.com>
2020-12-18 15:55:00 +0800

    Add test for day 17 puzzle 2

--- src/day17/day17.spec.ts
+++ src/day17/day17.spec.ts
@@ -1,5 +1,5 @@
 import { readFile } from "../helper";
-import { simulateCycles, getActiveCount } from './day17';
+import { simulateCycles, simulateCycles2, getActiveCount } from './day17';
 
 describe("day 17", () => {
   describe("puzzle 1", () => {
@@ -7,6 +7,14 @@ describe("day 17", () => {
       const input = readFile("inputs/day17/testInput.txt");
       const result = getActiveCount(simulateCycles(input, 6));
       expect(result).toBe(112);
+    });
+  });
+
+  describe("puzzle 2", () => {
+    it("test 1", () => {
+      const input = readFile("inputs/day17/testInput.txt");
+      const result = getActiveCount(simulateCycles2(input, 6));
+      expect(result).toBe(848);
     });
   });
 });
