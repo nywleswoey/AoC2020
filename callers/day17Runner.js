54f5df22bdcbd1a77a4117d04e5dcbfa67676e85
Selwyn Yeow <sgfjords@gmail.com>
2020-12-18 15:55:28 +0800

    Solve day 17 puzzle 2

--- callers/day17Runner.js
+++ callers/day17Runner.js
@@ -2,3 +2,5 @@ const { helpers, day17 } = require("../dist/index");
 
 const input = helpers.readFile("inputs/day17/puzzleInput.txt");
 console.log(day17.getActiveCount(day17.simulateCycles(input, 6)));
+
+console.log(day17.getActiveCount(day17.simulateCycles2(input, 6)));
--- src/day17/day17.ts
+++ src/day17/day17.ts
@@ -2,6 +2,7 @@ interface Point {
   x: number;
   y: number;
   z: number;
+  w?: number;
 }
 
 const getNeighbours = (x: number, y: number, z: number) => {
@@ -68,6 +69,80 @@ const simulateCycles = (input: string[], numCycles: number) => {
   return pointsMap;
 }
 
+const getNeighbours2 = (x: number, y: number, z: number, w: number) => {
+  const neighbours = new Array<Point>();
+
+  for (let l = -1; l < 2; l++) {
+    for (let k = -1; k < 2; k++) {
+      for (let j = -1; j < 2; j++) {
+        for (let i = -1; i < 2; i++) {
+          if (i === 0 && j === 0 && k === 0 && l === 0) {
+            continue;
+          }
+
+          neighbours.push({ x: x + i, y: y + j, z: z + k, w: w + l });
+        }
+      }
+    }
+  }
+
+  return neighbours;
+}
+
+const computeStatus2 = (x: number, y: number, z: number, w: number, pointsMap: Map<string, boolean>) => {
+  const neighbours = getNeighbours2(x, y, z, w);
+  const currentStatus = pointsMap.get(`${x},${y},${z},${w}`) ?? false;
+  const neighbourStatuses = neighbours.map(n => pointsMap.get(`${n.x},${n.y},${n.z},${n.w}`) ?? false);
+  const numOfActiveStatuses = neighbourStatuses.filter(s => s).length;
+  return currentStatus ?
+    (numOfActiveStatuses === 2 || numOfActiveStatuses === 3) // remain active if only 2 or 3 active neighbours
+    : numOfActiveStatuses === 3; // turn active if exactly 3 neighbours are active
+}
+
+const simulate2 = (pointsMap: Map<string, boolean>) => {
+  const newPointsMap = new Map<string, boolean>();
+  for (const [pt, _] of pointsMap) {
+    const [x, y, z, w] = pt.split(',').map(Number);
+    const neighbours = getNeighbours2(x, y, z, w);
+    for (const neighbour of neighbours) {
+      newPointsMap.set(`${neighbour.x},${neighbour.y},${neighbour.z},${neighbour.w}`, false);
+    }
+  }
+
+  for (const [pt, _] of newPointsMap) {
+    const [x, y, z, w] = pt.split(',').map(Number);
+    const newStatus = computeStatus2(x, y, z, w, pointsMap);
+    newPointsMap.set(pt, newStatus);
+  }
+
+  const t = new Map<string, boolean>();
+  for (const [pt, s] of newPointsMap) {
+    const [x, y, z, w] = pt.split(',').map(Number);
+    if (z === -1 && w === -1) {
+      t.set(pt, s);
+    }
+  }
+
+  return newPointsMap;
+}
+
+const simulateCycles2 = (input: string[], numCycles: number) => {
+  let pointsMap = new Map<string, boolean>();
+  for (let y = 0; y < input.length; y++) {
+    const row = input[y].split('');
+    for (let x = 0; x < row.length; x++) {
+      const isActive = row[x] === '#';
+      pointsMap.set(`${x},${y},0,0`, isActive);
+    }
+  }
+
+  for (let cycle = 0; cycle < numCycles; cycle++) {
+    pointsMap = simulate2(pointsMap);
+  }
+
+  return pointsMap;
+}
+
 const getActiveCount = (pointsMap: Map<string, boolean>) => {
   let count = 0;
   for (const [pt, status] of pointsMap) {
@@ -78,4 +153,4 @@ const getActiveCount = (pointsMap: Map<string, boolean>) => {
   return count;
 }
 
-export { simulateCycles, getActiveCount };
+export { simulateCycles, simulateCycles2, getActiveCount };
